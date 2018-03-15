977561587c5ddd38f0da0d409669d1d71c43d02f
Alan Liang <liangalann@gmail.com>
2018-03-15 22:01:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo("John"); {
+function happyHolidaysTo(John); {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo("Christmas" , "Chris") {
+function happyHolidayTo(Christmas , Chris) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown("Christmas" , 21) {
+function holidayCountdown(Christmas , 21) {
   return `It's ${days} days until ${holiday}!`
 }
